faa6c7038fbd52423fdbd14ad429de74440a4265
Vincent Davis <forklift@vdavis.net>
2024-01-09 13:26:15 -0700

    update intro

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,8 @@ st.set_page_config(layout="wide")
 """
 # Welcome to Vincent's experiments with cycling data files
 
-This is a work in progress. Please report any issues at [pyfitness_streamlit](https://github.com/vincentdavis/pyfitness_streamlit)
+You can also contact me on discord: [Vincent](discordapp.com/users/vincent.davis)
 
-You can also contact me on discord: [Vincent](discordapp.com/users/VincentDavis#3484)
-
-This "dashboard" will look at the details of the FIT file 
-and the distribution on the data. You are able to compare two file side by side.
 """
 
 st.write("Login to ZwiftPower")
