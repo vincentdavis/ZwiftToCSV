66a012a4bac977ced27eb952bee187403af346a2
Vincent <vincent@vincentdavis.net>
2024-01-09 07:23:19 -0700

    Initial commit

---
+++ streamlit_app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+"""
+# Welcome to Vincent's experiments with cycling data files
+
+This is a work in progress. Please report any issues at [pyfitness_streamlit](https://github.com/vincentdavis/pyfitness_streamlit)
+
+You can also contact me on discord: [Vincent](discordapp.com/users/VincentDavis#3484)
+
+This "dashboard" will look at the details of the FIT file 
+and the distribution on the data. You are able to compare two file side by side.
+"""
