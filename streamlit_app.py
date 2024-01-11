99fa97c8217a6974e82e7f50bca7e3889639b241
Vincent Davis <forklift@vdavis.net>
2024-01-11 11:21:47 -0700

    change home page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 from st_pages import Page, show_pages
 
-from zp_fetch import ZPSession
-
 st.set_page_config(page_title="Zwift to CSV")
 show_pages(
     [
@@ -16,36 +14,7 @@ show_pages(
 """
 # Zwift to CSV utility
 
-If you have questions or feature request, DM me at [Vincent](discordapp.com/users/vincent.davis)
+If you have questions, problems or feature request, DM me at [Vincent](discordapp.com/users/vincent.davis)
 
+Checkout the option on the left.
 """
-
-st.write("Login to ZwiftPower using your Zwift account. No data is stored.")
-username = None
-password = None
-if username is None:
-    username = st.text_input(label="UserName", placeholder="username")
-if password is None:
-    password = st.text_input(label="Password", placeholder="password")
-
-zps = ZPSession(login_data={"username": username, "password": password})
-
-event_url = None
-cat = None
-format = None
-if event_url is None:
-    cat = st.radio(label="Category", options=["ALL", "A", "B", "C", "D", "E"])
-    format = st.radio(label="Format", options=["msec", "elapsed"])
-    event_url = st.text_input(
-        label="Event URL (may take a few seconds)", placeholder="https://zwiftpower.com/events.php?zid=4073983"
-    )
-if event_url and cat and format:
-    cat = cat.replace("ALL", "")
-    df = zps.get_primes_from_url(event_url, cat, format)
-    st.download_button(
-        label="Download csv file",
-        data=df.to_csv(index=False).encode("utf-8"),
-        file_name=f"Event_prime_ID_{id}_CAT_{cat}_FORMAT_{format}.csv",
-        mime="text/csv",
-    )
-    st.dataframe(df)
